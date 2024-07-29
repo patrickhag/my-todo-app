76ff82452c5a5d706cdbffb82fe4add4af0f17e1
patrickhag <patrickhag09@gmail.com>
2024-07-29 15:33:17 +0200

    finish setting env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 'use client'
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 function Home() {
   return (
-    <div>
+    <div className='container'>
       <h1>hello world</h1>
+      <Button>Test shadcn</Button>
     </div>
   )
 }
